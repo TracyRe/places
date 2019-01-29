829ccb33bd32154a8680fcb040da7d900457dc0d
Aurora Shido-Wagenet and Tracy Jo Reith <aurora+tracy@>
2019-01-28 16:35:14 -0800

    Take input and put location name into card header and properties into list in card details div

--- js/scripts.js
+++ js/scripts.js
@@ -22,7 +22,7 @@ function Place(name, landmarks, people, time, notes){
   this.notes = notes
 }
 
-$(function(){
+$(document).ready(function() {
   var newPlacesDb = new PlacesDatabase();
   $("#places").submit(function(event){
     event.preventDefault();
@@ -30,22 +30,6 @@ $(function(){
     newPlacesDb.addPlace(newPlace);
 
     // $("#result").append("<button onclick=\"alert('hello');\">" + newPlace.name + "</button>");
-    $("#placeButtons").append("<button value=" + newPlace.id + ">" + newPlace.name + "</button>");
-    $("button").click(function(){
-      var buttonValue = this.value;
-      newPlacesDb.places.forEach(function(place){
-        var display = false;
-        console.log(display);
-        if ((display === false) && (buttonValue == place.id)){
-         $("#placeInfo").append("<h4>" + place.name + "</h4>" + "<ul><li>" + place.landmarks + "</li>" + "<li>" + place.people + "</li>" + "<li>" + place.time + "</li>" + "<li>" + place.notes + "</li></ul>");
-        }
-        display = true;
-        console.log(display);
+    $("#result").append("<div class=\"place-card\" id=" + newPlace.name + "-card" + "div class=\"place-head\">" + newPlace.name + "</div>  <div class=\"place-details\" <ul><li>" + place.landmarks + "</li> <li>" + place.people + "</li> <li>" + place.time + "</li><li>" + place.notes + "</li> </ul> </div></div>");
       });
-      // console.log(this.value);
-      // console.log(newPlace.value); UNDEFINED
-      // console.log(newPlace.id.value);  UNDEFINED
-    });
   });
-
-});
